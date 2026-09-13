02adc91e3d0842100641ed45ad12365ca22c4c94
ossamasal2012 <ossamasal2012@gmail.com>
2026-09-13 06:57:11 +0400

    Update AirQualitySummaryCard.kt

--- app/src/main/kotlin/com/osama/weather/ui/components/AirQualitySummaryCard.kt
+++ app/src/main/kotlin/com/osama/weather/ui/components/AirQualitySummaryCard.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
