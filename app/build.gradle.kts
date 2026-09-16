51a53685d24fd526367d7fc3fd32582cebe4277a
ossamasal2012 <ossamasal2012@gmail.com>
2026-09-16 21:07:49 +0400

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,8 +39,8 @@ android {
         // Bump versionCode by exactly +1 on every release you push. See the
         // project README for the full release checklist.
         // ---------------------------------------------------------------------
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
